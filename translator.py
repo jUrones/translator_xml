972af3696cffc74e7cbc660722d6664939104c8c
jUrones <javierum98@gmail.com>
2019-02-01 10:49:36 +0100

    Se resuelven problemas menores

--- translator.py
+++ translator.py
@@ -1,25 +1,21 @@
-# coding: utf-8
+
 from __future__ import print_function
 
 from googletrans import Translator
 import xml.etree.ElementTree as ET  
-import pprint # Para poder hacer uso de PrettyPrinter
-		
+import pprint # Para poder hacer uso de PrettyPrinter		
 def main():
 	translator = Translator()
-	tree = getTree()  
+	tree = ET.parse('book.xml')
 	root = tree.getroot()
-	
+
 	translated = tree
-	pp = pprint.PrettyPrinter(indent=2)
-	
+
 	for elem in root:
 		for subelem in elem:
-			subelem.text = translator.translate(subelem.text, dest='es').text
+			#subelem.text = translator.translate(subelem.text, dest='es').text
+			print(subelem.text)
 	tree.write("XMLtranslated.xml")
 
-def getTree():
-	fileName = raw_input("Insert the name of XML file or the file path: \n")
-	return ET.parse(fileName);
 if __name__ == '__main__':
     main()
