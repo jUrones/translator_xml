89da89ec42c0c3faebcf2b2e7987218a6291b0d9
jUrones <javierum98@gmail.com>
2019-02-01 19:57:53 +0100

    Add new simple XML file for testing the application/script. The funcionality of the application has been changed, add a new menu to select the different languages. Now the application allows to translate from whatever language to spanish, french, italian or english. When the script finish, it creates a new file called XMLtranslated_language.xml, language could be 'es', 'fr', 'it' or 'en'.

--- translator.py
+++ translator.py
@@ -3,19 +3,56 @@ from __future__ import print_function
 
 from googletrans import Translator
 import xml.etree.ElementTree as ET  
-import pprint # Para poder hacer uso de PrettyPrinter		
+import sys
+
 def main():
 	translator = Translator()
-	tree = ET.parse('book.xml')
+	tree = getTree()
 	root = tree.getroot()
 
-	translated = tree
+	language = getLanguage()
 
 	for elem in root:
-		for subelem in elem:
-			#subelem.text = translator.translate(subelem.text, dest='es').text
-			print(subelem.text)
-	tree.write("XMLtranslated.xml")
+            elem.text = translator.translate(elem.text, dest=language).text
+            for subelem in elem:
+                subelem.text = translator.translate(subelem.text, dest=language).text
+
+	tree.write("XMLtranslated_"+language+".xml")
+
+
+def getTree():
+	fileName = raw_input("Insert the name of XML file or the file path: \n")
+	return ET.parse(fileName);
+
+def getLanguage():
+    showOptions()
+    try:
+        language = int(raw_input('Select language number (1...4)\n'))
+	if(language < 1 or language > 4):
+	    print('No valid option')
+	    print('Closing program....')
+	    sys.exit()
+	if(language == 1):
+            return 'es'
+        if(language== 2):
+            return 'fr'
+        if(language == 3):
+            return 'it'
+        if(language== 4):
+            return 'en'           
+    except:
+	 print('No valid option')
+	 print('Closing program....')
+	 sys.exit()
+
+def showOptions():
+    print('---------Select the language to translate--------')
+    print('\t1...Spanish')
+    print('\t2...French')
+    print('\t3...Italian')
+    print('\t4...English')
+
+
 
 if __name__ == '__main__':
     main()
